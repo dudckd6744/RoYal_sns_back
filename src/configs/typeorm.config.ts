b3e2fc6692b629483b186ee5d368a6c9712df881
dudckd6744 <dudckd6744@naver.com>
2021-08-27 21:13:50 +0900

    decker save

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -7,13 +7,13 @@ export const typeORMConfig: TypeOrmModuleOptions = {
     type: "mysql",
     host: '172.18.0.2',
     port: 3306,
-    username: 'test',
-    password: 'qwerasdf',
-    database: 'bolier',
+    username: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
     autoLoadEntities: true,
     timezone: '+09:00',
-    connectTimeout: 30000,
-    acquireTimeout: 30000
+    connectTimeout: 300000,
+    acquireTimeout: 300000
 }
