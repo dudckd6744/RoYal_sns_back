094ceb5aacc755fcb22d0724a5ff250d837c9b29
Bakss <dudckd6744@naver.com>
2021-09-03 16:06:37 +0900

    test58

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -3,18 +3,14 @@
 # cd /home/ubuntu/deploy/bolier-CD
 # ./deploy.sh > /dev/null 2> /dev/null < /dev/null &
 echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" 
-CURRENT_PID=$(sudo docker container ls -q)
 
-docker-compose down
+`docker-compose down`
 
-docker rmi $(docker ps -a -q)
+`docker rmi $(docker ps -a -q)`
 
-docker volume prune
+`docker volume prune`
 
-docker rm $(docker ps -a -q)
-
-
-
+`docker rm $(docker ps -a -q)`
 
 cd /home/ubuntu/deploy/bolier-CD     # 해당 디렉토리로 이동 (Dockerfile 을 해당 디렉토리에 옮겼기 때문에)
 docker-compose up --build       # Docker Image 생성 
