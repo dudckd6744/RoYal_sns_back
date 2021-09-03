d07e28c76cb2120448f2c1a28fac6aae4ffbec5e
Bakss <dudckd6744@naver.com>
2021-09-03 15:15:55 +0900

    test56

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -5,14 +5,16 @@
 echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" 
 CURRENT_PID=$(sudo docker container ls -q)
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." 
-else
-  echo "> sudo docker stop $CURRENT_PID"   # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
-  sudo docker stop $CURRENT_PID
-  sleep 5
-fi
+docker-compose down
+
+docker rmi $(docker ps -a -q)
+
+docker volume prune
+
+docker rm $(docker ps -a -q)
+
+
+
 
 cd /home/ubuntu/deploy/bolier-CD     # 해당 디렉토리로 이동 (Dockerfile 을 해당 디렉토리에 옮겼기 때문에)
 docker-compose up --build       # Docker Image 생성 
