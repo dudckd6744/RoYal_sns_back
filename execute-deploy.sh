c39332c6a6b3c114442d06000b2d8a331d29f98b
Bakss <dudckd6744@naver.com>
2021-09-04 11:55:22 +0900

    test-01

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -16,4 +16,4 @@ echo "> 현재 실행 중인 Docker 컨테이너 pid 확인"
 `docker rm $(docker ps -a -q)`
 
 cd /home/ubuntu/deploy/bolier-CD     # 해당 디렉토리로 이동 (Dockerfile 을 해당 디렉토리에 옮겼기 때문에)
-docker-compose up --build       # Docker Image 생성 
+docker-compose up -d --build       # Docker Image 생성 
