b04ed341e205e2daa6a0f7f0c185e1f299748585
Bakss <dudckd6744@naver.com>
2021-09-04 10:35:35 +0900

    test-65

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,20 +1,19 @@
 # #!/bin/bash
 
-cd /home/ubuntu/deploy/bolier-CD
-./deploy.sh > /dev/null 2> /dev/null < /dev/null &
+# cd /home/ubuntu/deploy/bolier-CD
+# ./deploy.sh > /dev/null 2> /dev/null < /dev/null &
 
 
 
-# echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" 
+echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" 
 
-# `docker-compose down`
+`docker-compose down`
 
-# `docker rmi $(docker ps -a -q)`
+`docker rmi $(docker ps -a -q)`
 
-# `docker volume prune`
+`docker volume prune`
 
-# `docker rm $(docker ps -a -q)`
+`docker rm $(docker ps -a -q)`
 
-# cd /home/ubuntu/deploy/bolier-CD     # 해당 디렉토리로 이동 (Dockerfile 을 해당 디렉토리에 옮겼기 때문에)
-# docker-compose up --build       # Docker Image 생성 
-# # sudo docker run -d -p 5000:5000 dudckd  # Docker Container 생성
+cd /home/ubuntu/deploy/bolier-CD     # 해당 디렉토리로 이동 (Dockerfile 을 해당 디렉토리에 옮겼기 때문에)
+docker-compose up --build       # Docker Image 생성 
