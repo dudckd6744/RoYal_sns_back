8c59cf69387987722e9d7d0daa25b3f8ca943ed9
Bakss <dudckd6744@naver.com>
2021-09-04 10:27:01 +0900

    test-64

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT
+  const port = process.env.PORT || 5000
 
   await app.listen(port);
 }
